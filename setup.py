fbce5139ef343ad49ff7b936c011c9bfebba4116
orionw <wellerorion@gmail.com>
2019-08-29 18:34:17 -0600

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name="configtune",
-	version="0.0.7",
+	version="0.0.8",
 	description="A package to tune parameters with config files: designed with machine learning hyperparameter tuning in mind.",
 	long_description=None,
 	url= "https://github.com/orionw/configtune",
@@ -12,7 +12,7 @@ setup(
 	classifiers=[
 		"Programming Language :: Python :: 3.5",
 	],
-    download_url="https://github.com/orionw/configtune/archive/v0.0.7.tar.gz",
+    download_url="https://github.com/orionw/configtune/archive/v0.0.8.tar.gz",
 	keywords="tuning machinelearning genetic hyperparameters bayesian optimization",
 	packages=find_packages(),
 	install_requires=["deap", "numpy", "pandas", "scikit-optimize"],
